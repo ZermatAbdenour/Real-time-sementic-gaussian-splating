cfd20f8011b2bdca1ec56e98ad85b78a0fc8ff48
ZermatAbdenour <a_zermat@estin.dz>
2026-03-11 06:57:34 +0100

    config changes to optimize performance.

--- RTSGS/Config/ReplicaConfig.py
+++ RTSGS/Config/ReplicaConfig.py
@@ -13,9 +13,9 @@ class ReplicaConfig(Config):
         self.set('height', 680)
         self.set('depth_scale', 6553.5) 
 
-        self.set('voxel_size', 0.02)
-        self.set("sigma_px", 2.0)
-        self.set("sigma_z0", 0.001)
+        self.set('voxel_size', 0.03)
+        self.set("sigma_px", 4.0)
+        self.set("sigma_z0", 0.003)
         self.set("sigma_z1", 0.0)
         self.set("alpha_init", 1.0)
         self.set("alpha_min", 0.01)
